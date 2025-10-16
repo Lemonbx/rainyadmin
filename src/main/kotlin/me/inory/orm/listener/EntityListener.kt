bd7864fb3e259bc16bfdcee625ebd77ce9b9905b
Rainy <34757199+Lemonbx@users.noreply.github.com>
2025-10-16 10:13:14 +0800

    Replace Date with LocalDateTime in EntityListener

--- src/main/kotlin/me/inory/orm/listener/EntityListener.kt
+++ src/main/kotlin/me/inory/orm/listener/EntityListener.kt
@@ -6,27 +6,29 @@ import me.inory.orm.base.BaseEntityDraft
 import org.babyfish.jimmer.kt.isLoaded
 import org.babyfish.jimmer.sql.DraftInterceptor
 import org.springframework.stereotype.Component
-import java.util.*
+import java.time.LocalDateTime
 
 @Component
 class EntityListener : DraftInterceptor<BaseEntity, BaseEntityDraft> {
     override fun beforeSave(draft: BaseEntityDraft, original: BaseEntity?) {
         super.beforeSave(draft, original)
-        draft.updateTime = Date()
-        if (StpUtil.isLogin()){
-            draft.updateBy = StpUtil.getLoginIdAsLong()
-        }
-//        if (!isLoaded(draft, BaseEntity::requestId)) {
-//            draft.requestId = UrlInterceptor.operLogInfoThreadLocal.get()?.requestId
-//        }
+        draft.updateTime = LocalDateTime.now()
+        try{
+            if (StpUtil.isLogin()){
+                draft.updateBy = StpUtil.getLoginIdAsLong()
+            }
+        }catch (_: Exception){}
+
         if (original == null) {
             if (!isLoaded(draft, BaseEntity::createTime)) {
-                draft.createTime = Date()
+                draft.createTime = LocalDateTime.now()
             }
             if (!isLoaded(draft, BaseEntity::createBy)) {
-                if (StpUtil.isLogin()){
-                    draft.createBy = StpUtil.getLoginIdAsLong()
-                }
+                try{
+                    if (StpUtil.isLogin()){
+                        draft.createBy = StpUtil.getLoginIdAsLong()
+                    }
+                }catch (_: Exception){}
             }
         }
     }
