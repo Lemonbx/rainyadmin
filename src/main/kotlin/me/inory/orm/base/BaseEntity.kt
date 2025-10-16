d87b0630dcfdf036091e480f89dba89459913218
Rainy <34757199+Lemonbx@users.noreply.github.com>
2025-10-16 10:12:03 +0800

    Change date types to LocalDateTime in BaseEntity

--- src/main/kotlin/me/inory/orm/base/BaseEntity.kt
+++ src/main/kotlin/me/inory/orm/base/BaseEntity.kt
@@ -1,12 +1,12 @@
 package me.inory.orm.base
 
 import org.babyfish.jimmer.sql.MappedSuperclass
-import java.util.Date
+import java.time.LocalDateTime
 
 @MappedSuperclass
 interface BaseEntity {
-    val createTime: Date
-    val updateTime: Date
+    val createTime: LocalDateTime
+    val updateTime: LocalDateTime
     val createBy: Long?
     val updateBy: Long?
 }
